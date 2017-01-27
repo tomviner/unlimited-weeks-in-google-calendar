7d2ff07754e2d4fe7d4dcff1b20f94932d748920
Tom Viner <tom@viner.tv>
2017-01-27 22:56:01 +0000

    beautify

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,19 +1,19 @@
-// Written in ECMAScript 6
+/* jshint esversion: 6 */
 
-function trigger(event_names, elem){
+function trigger(event_names, elem) {
     // event_names: space sep names of events
     // elem: jQuery element
     if (!event_names || event_names.length === 0) {
         console.log(elem)
-        throw(`Cannot trigger ${event_names}, element event_names`)
+        throw (`Cannot trigger ${event_names}, element event_names`)
     }
     if (!elem || elem.length === 0) {
         console.log(elem)
-        throw(`Cannot trigger ${event_names}, element missing`)
+        throw (`Cannot trigger ${event_names}, element missing`)
     }
     for (let event_name of event_names.split(' ')) {
         let evt = document.createEvent("MouseEvents")
-        // eek, there's gotta be a better way!
+            // eek, there's gotta be a better way
         evt.initMouseEvent(event_name, true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
         let dom_elem = elem.get(0)
         dom_elem.dispatchEvent(evt)
@@ -23,13 +23,23 @@ function trigger(event_names, elem){
 
 
 class Toolbar {
-    get month_view () {return $('#topRightNavigation .goog-imageless-button').eq(2)}
-    get custom_view () {return $('#topRightNavigation .goog-imageless-button').eq(3)}
-    get prev_month () {return $('.navBack').eq(0)}
-    get today () {return $('#todayButton\\:1,#todayButton\\:2').children().eq(0)}
-    get next_month () {return $('#dp_0_next')}
+    get month_view() {
+        return $('#topRightNavigation .goog-imageless-button').eq(2)
+    }
+    get custom_view() {
+        return $('#topRightNavigation .goog-imageless-button').eq(3)
+    }
+    get prev_month() {
+        return $('.navBack').eq(0)
+    }
+    get today() {
+        return $('#todayButton\\:1,#todayButton\\:2').children().eq(0)
+    }
+    get next_month() {
+        return $('#dp_0_next')
+    }
 
-    poll_custom_button_visibility(wait_ms=500) {
+    poll_custom_button_visibility(wait_ms = 500) {
         if (this.custom_view.is(":visible")) {
             $(document).trigger("custom_view_buttons_visible")
         } else {
@@ -52,72 +62,99 @@ class Toolbar {
 }
 
 class BigCal {
-    get first_day_num () {
+    get first_day_num() {
         return parseInt(
             $('#gridcontainer span[class^="ca-cdp"]')
-                .attr('class')
-                .split('ca-cdp')
-                .slice(-1)[0]
+            .attr('class')
+            .split('ca-cdp')
+            .slice(-1)[0]
         )
     }
 }
 
 class MiniCal {
-    get cells () {return $('.dp-cell[class*="dp-o"]')}
-    nth(n) {return this.cells.eq(n)}
-    get first () {return this.nth(0)}
-    get first_day_num () {return this.extract_day_num(this.first)}
-    get last () {return this.nth(7 * 6 - 1)}
-    get last_day_num () {return this.extract_day_num(this.last)}
-    get month_start_indexes () {return this.cells.map((i, el) => {
-            if ($(el).text() === '1'){
+    get cells() {
+        return $('.dp-cell[class*="dp-o"]')
+    }
+    nth(n) {
+        return this.cells.eq(n)
+    }
+    get first() {
+        return this.nth(0)
+    }
+    get first_day_num() {
+        return this.extract_day_num(this.first)
+    }
+    get last() {
+        return this.nth(7 * 6 - 1)
+    }
+    get last_day_num() {
+        return this.extract_day_num(this.last)
+    }
+    get month_start_indexes() {
+        return this.cells.map((i, el) => {
+            if ($(el).text() === '1') {
                 return i
             } else {
                 return null
             }
         })
     }
-    get month_start_cells () {return this.cells.map((i, el) => {
-            if ($(el).text() === '1'){
+    get month_start_cells() {
+        return this.cells.map((i, el) => {
+            if ($(el).text() === '1') {
                 return $(el)
             } else {
                 return null
             }
         })
     }
-    get month_starts_high () {return this.month_start_indexes[0] < 7}
-    get month_ends_low () {return this.month_start_indexes[1] >= 7 * 5}
-    get weeks_in_month () {
+    get month_starts_high() {
+        return this.month_start_indexes[0] < 7
+    }
+    get month_ends_low() {
+        return this.month_start_indexes[1] >= 7 * 5
+    }
+    get weeks_in_month() {
         // bools get cast to 0 or 1 here. each true is an extra week
         return 3 + this.month_starts_high + this.month_ends_low
     }
-    get selected () {return this.cells.filter('[class*="-selected"]')}
-    cell_from_day_num (day_num) {return this.cells.filter(`[id$="${day_num}"]`)}
-    extract_day_num (el) {return parseInt(el.eq(0).attr('id').split('_').slice(-1)[0])}
-    month_backward () {trigger('mousedown mouseup', $('.dp-sb-prev'))}
-    month_forward () {trigger('mousedown mouseup', $('.dp-sb-next'))}
-    navigate_to (day_num) {
+    get selected() {
+        return this.cells.filter('[class*="-selected"]')
+    }
+    cell_from_day_num(day_num) {
+        return this.cells.filter(`[id$="${day_num}"]`)
+    }
+    extract_day_num(el) {
+        return parseInt(el.eq(0).attr('id').split('_').slice(-1)[0])
+    }
+    month_backward() {
+        trigger('mousedown mouseup', $('.dp-sb-prev'))
+    }
+    month_forward() {
+        trigger('mousedown mouseup', $('.dp-sb-next'))
+    }
+    navigate_to(day_num) {
         let i = 0
-        while (day_num < this.first_day_num || this.last_day_num < day_num){
-            if (++i > 10){
+        while (day_num < this.first_day_num || this.last_day_num < day_num) {
+            if (++i > 10) {
                 throw "Too many loops"
             }
-            if (day_num < this.first_day_num){
+            if (day_num < this.first_day_num) {
                 this.month_backward()
-            } else if (this.last_day_num < day_num){
+            } else if (this.last_day_num < day_num) {
                 this.month_forward()
             } else {
                 throw 'unknown condition'
             }
         }
-        if (this.cell_from_day_num(day_num).length != 1){
+        if (this.cell_from_day_num(day_num).length != 1) {
             throw "target not found on mini cal"
         }
     }
 }
 
-function set_range(weeks_left){
-    let weeks_wanted = weeks_left
+function set_range(weeks_left) {
     let target_start_day_num = big_cal.first_day_num
 
     // move to month view, click doesn't work here
@@ -128,14 +165,14 @@ function set_range(weeks_left){
 
     // do a double manoeuvre: click next month during a click drag over the mini calendar.
     // this is how we reach more than one month
-    trigger('mousedown', mini_cal.nth(mini_cal.month_start_indexes[0]+7))
+    trigger('mousedown', mini_cal.nth(mini_cal.month_start_indexes[0] + 7))
     let days = 0
     let i = -1
     while (weeks_left > 0) {
         i++
         console.log(`${weeks_left} weeks left,`)
         let weeks_in_month = mini_cal.weeks_in_month
-        if (weeks_in_month > weeks_left){
+        if (weeks_in_month > weeks_left) {
             console.log(`    < ${weeks_in_month} months - no full months left`)
             break
         }
@@ -143,10 +180,9 @@ function set_range(weeks_left){
         console.log(`    - ${weeks_in_month} weeks_in_month`)
         mini_cal.month_forward()
     }
-    if (weeks_left === 0){
+    if (weeks_left === 0) {
         console.log(`    = exactly no weeks_left`)
-    }
-    else if (weeks_left < 0) {
+    } else if (weeks_left < 0) {
         throw `Didn't expect ${weeks_left} weeks_left`
     } else if (weeks_left > 0) {
         days = 7 * weeks_left
@@ -168,17 +204,15 @@ function set_range(weeks_left){
 
     // move active range forward, out the way
     mini_cal.month_forward()
-    // we must click outside the active range, otherwise, we just select a single day
+        // we must click outside the active range, otherwise, we just select a single day
     trigger('mousedown mouseup', mini_cal.last)
 
     // now click the date we want, in the mini map
     mini_cal.navigate_to(target_start_day_num)
     trigger('mousedown mouseup', mini_cal.cell_from_day_num(target_start_day_num))
-
-
 }
 
-function set_weeks(weeks){
+function set_weeks(weeks) {
     let weeks_before = $('.month-row').length
     set_range(weeks)
     let weeks_after = $('.month-row').length
@@ -202,15 +236,14 @@ let mini_cal = new MiniCal()
 let big_cal = new BigCal()
 let toolbar = new Toolbar()
 
-
-if (demo===true){
+if (demo === true) {
     $(document)
         .on("custom_view_buttons_visible", toolbar.inject_buttons)
 } else {
     $(document)
         .on("custom_view_buttons_visible", function() {
             toolbar.inject_buttons()
-            // demo
+                // demo
             setTimeout(inc_week, 1000)
             setTimeout(inc_week, 1500)
             setTimeout(dec_week, 3000)
